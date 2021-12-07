8ddc0d4e81c7d3a2ae371e2140d36feda98925a6
LaranGod <miguel05amancio2000@gmail.com>
2021-12-07 01:47:00 -0300

    add password validation

--- lib/ui/screens/fourth_screen.dart
+++ lib/ui/screens/fourth_screen.dart
@@ -1,7 +1,6 @@
 import 'package:economize_combustivel/ui/widgets/fourth_screen/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:economize_combustivel/ui/widgets/header.dart';
-import 'package:ionicons/ionicons.dart';
 
 class FourthScreen extends StatelessWidget {
   const FourthScreen({Key? key}) : super(key: key);
@@ -10,13 +9,34 @@ class FourthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Theme.of(context).backgroundColor,
-      child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          physics: const BouncingScrollPhysics(),
-          children: [
-            Header(text: 'Perfil'),
-            SizedBox(height: 256, child: TextFieldGeneral()),
-          ]),
+      child: ListView(physics: const BouncingScrollPhysics(), children: [
+        const Padding(
+          padding: EdgeInsetsDirectional.only(start: 16, end: 16),
+          child: Header(text: 'Perfil'),
+        ),
+
+        /* const Padding(
+          padding: EdgeInsetsDirectional.only(start: 16, end: 16),
+          child: Card(
+            child: SizedBox(
+              height: 30,
+              child: Align(
+                alignment: Alignment(-0.96, 0),
+                child: Text(
+                  'Acesse sua conta',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            color: Color(0xFF082032),
+          ),
+        ), */
+        Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+          child: SizedBox(height: 400, child: TextFieldGeneral()),
+        ),
+        // SizedBox(height: 13, child: Text('teste')),
+      ]),
     );
   }
 }
--- lib/ui/widgets/fourth_screen/text_field.dart
+++ lib/ui/widgets/fourth_screen/text_field.dart
@@ -7,66 +7,175 @@ class TextFieldGeneral extends StatefulWidget {
 }
 
 class _TextFieldGeneralState extends State<TextFieldGeneral> {
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
   final emailController = TextEditingController();
-  String password = '';
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  bool isConfirmPasswordVisible = false;
   bool isPasswordVisible = false;
+  bool isCreateAccount = false;
+  bool isLoginError = false;
   @override
   void initState() {
     super.initState();
     emailController.addListener(() => setState(() {}));
+    passwordController.addListener(() => setState(() {}));
+    confirmPasswordController.addListener(() => setState(() {}));
+  }
+
+  void clearText() {
+    emailController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+    setState(() {
+      isConfirmPasswordVisible = false;
+      isPasswordVisible = false;
+    });
   }
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: ListView(
-          padding: EdgeInsets.all(10),
-          children: [
-            buildEmail(),
-            const SizedBox(height: 10),
-            buildPassword(),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.deepOrange,
+  Widget build(BuildContext context) => Form(
+        key: _form,
+        child: Center(
+          child: ListView(
+            children: [
+              isCreateAccount
+                  ? const SizedBox(height: 0)
+                  : const SizedBox(height: 28),
+              buildEmail(),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: isLoginError ? 70 : 50,
+                child: SizedBox(
+                  child: buildPassword(),
+                ),
               ),
-              child: const Text('Confirmar'),
-              onPressed: () {
-                print('Email: ${emailController.text}');
-                print('Password: $password');
-              },
-            ),
-          ],
+              const SizedBox(height: 10),
+              SizedBox(
+                height: isCreateAccount ? 70 : 0,
+                child: SizedBox(
+                  child: isCreateAccount
+                      ? buildPasswordConfirmation()
+                      : (const SizedBox(height: 0)),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.deepOrange,
+                ),
+                child:
+                    isCreateAccount ? Text('Criar conta') : Text('Fazer login'),
+                onPressed: () {
+                  _form.currentState!.validate();
+                  print('Email: ${emailController.text}');
+                  print('Password: ${passwordController.text}');
+                },
+              ),
+              const SizedBox(height: 6),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                InkWell(
+                    child: isCreateAccount
+                        ? const Text('Já tem uma conta?')
+                        : const Text('Criar uma conta'),
+                    onTap: () => {
+                          setState(() => isCreateAccount = !isCreateAccount),
+                          clearText(),
+                        }),
+              ]),
+            ],
+          ),
         ),
       );
 
-  Widget buildEmail() => TextField(
-        controller: emailController,
-        decoration: InputDecoration(
-          hintText: 'name@example.com',
-          labelText: 'Email',
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Ionicons.mail_outline),
-        ),
-        keyboardType: TextInputType.emailAddress,
-        textInputAction: TextInputAction.done,
-      );
-  Widget buildPassword() => TextField(
-        onChanged: (value) => setState(() => this.password = value),
-        onSubmitted: (value) => setState(() => this.password = value),
-        decoration: InputDecoration(
-          hintText: '********',
-          labelText: 'Senha',
-          border: OutlineInputBorder(),
-          prefixIcon: Icon(Ionicons.lock_closed_outline),
-          suffixIcon: IconButton(
-            icon: isPasswordVisible
-                ? Icon(Ionicons.eye_off_outline)
-                : Icon(Ionicons.eye_outline),
-            onPressed: () =>
-                setState(() => isPasswordVisible = !isPasswordVisible),
+  Widget buildEmail() => SizedBox(
+        height: 50,
+        child: TextFormField(
+          controller: emailController,
+          decoration: InputDecoration(
+            hintText: 'name@example.com',
+            labelText: 'Email',
+            labelStyle: const TextStyle(color: Colors.white),
+            contentPadding:
+                const EdgeInsets.only(left: 0, right: 0, bottom: 15, top: 15),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+            prefixIcon: const Icon(Ionicons.mail_outline, size: 20),
           ),
+          keyboardType: TextInputType.emailAddress,
+          textInputAction: TextInputAction.done,
         ),
-        obscureText: !isPasswordVisible,
-        textInputAction: TextInputAction.done,
+      );
+
+  Widget buildPassword() => SizedBox(
+        height: 50,
+        child: TextFormField(
+          controller: passwordController,
+          validator: (value) {
+            setState(() => isLoginError = false);
+            if (value!.isEmpty) {
+              setState(() => isLoginError = true);
+              return 'Campo obrigatório';
+            }
+          },
+          decoration: InputDecoration(
+            hintText: '********',
+            labelText: 'Senha',
+            labelStyle: const TextStyle(color: Colors.white),
+            contentPadding:
+                const EdgeInsets.only(left: 0, right: 0, bottom: 15, top: 15),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+            prefixIcon: const Icon(Ionicons.lock_closed_outline, size: 20),
+            suffixIcon: IconButton(
+              icon: isPasswordVisible
+                  ? const Icon(
+                      Ionicons.eye_off_outline,
+                      size: 20,
+                    )
+                  : const Icon(
+                      Ionicons.eye_outline,
+                      size: 20,
+                    ),
+              onPressed: () =>
+                  setState(() => isPasswordVisible = !isPasswordVisible),
+            ),
+          ),
+          obscureText: !isPasswordVisible,
+          textInputAction: TextInputAction.done,
+        ),
+      );
+
+  Widget buildPasswordConfirmation() => SizedBox(
+        height: 50,
+        child: TextFormField(
+          controller: confirmPasswordController,
+          validator: (val) {
+            if (val!.isEmpty) {
+              return 'Campo obrigatório';
+            }
+            if (val != passwordController.text) {
+              return 'Senhas não conferem';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            hintText: '********',
+            labelText: 'Confirmar senha',
+            labelStyle: const TextStyle(color: Colors.white),
+            contentPadding:
+                const EdgeInsets.only(left: 0, right: 0, bottom: 15, top: 15),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(0)),
+            prefixIcon: const Icon(Ionicons.lock_closed_outline, size: 20),
+            suffixIcon: IconButton(
+              icon: isConfirmPasswordVisible
+                  ? const Icon(Ionicons.eye_off_outline, size: 20)
+                  : const Icon(Ionicons.eye_outline, size: 20),
+              onPressed: () => setState(
+                  () => isConfirmPasswordVisible = !isConfirmPasswordVisible),
+            ),
+          ),
+          obscureText: !isConfirmPasswordVisible,
+          textInputAction: TextInputAction.done,
+        ),
       );
 }
